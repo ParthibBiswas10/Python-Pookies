581e287d17485766b113b907a657d232df001ba1
ParthibBiswas10 <parthiblm10@gmail.com>
2024-10-24 23:36:20 +0530

    rearrange

---
+++ modifyRearrange.py
@@ -0,0 +1,28 @@
+class Solution:
+    def modifyAndRearrangeArr(self, arr):
+        n = len(arr)
+        
+        
+        for i in range(n - 1):
+            if arr[i] != 0 and arr[i] == arr[i + 1]:
+                arr[i] = arr[i] * 2  
+                arr[i + 1] = 0       
+        
+        
+        result = [num for num in arr if num != 0]  
+        result += [0] * (n - len(result))          
+        for i in range(n):
+            arr[i] = result[i]
+
+        return arr
+
+
+if __name__ == "__main__":
+    t = int(input()) 
+    while t > 0:
+        arr = list(map(int, input().split()))  
+        ob = Solution() 
+        ans = ob.modifyAndRearrangeArr(arr)  
+        print(*ans)  
+        print("~")  
+        t -= 1
